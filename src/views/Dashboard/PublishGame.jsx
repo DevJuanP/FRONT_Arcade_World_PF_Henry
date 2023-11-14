7c9d115ce2c5b1cc65da5bc3a49b2fda4968c52d
JuanP <juanpajueloch@gmail.com>
2023-11-13 22:28:50 -0500

    post videogame

--- src/views/Dashboard/PublishGame.jsx
+++ src/views/Dashboard/PublishGame.jsx
@@ -4,175 +4,206 @@ import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import { gameGenres, gamePlataforms, createVideogame } from '../../../src/redux/actions.js';
 
-const validate=(initialValues) => {
-  const errors = {};
-  const lettersOrSpacesREGEX = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-  const numberREGEX = /^([0-9]+(?:\.[0-9]*)?)$/;
-
-  if (initialValues.genreIds.length === 0) {
-    errors.genreIds = "Please insert videogame genre.";
-  } else if (initialValues.genreIds.length > 6) {
-    errors.genreIds = "You cannot select more than 6 types of genreIds";
-  }
-  
-  if (initialValues.platformIds.length === 0) {
-  errors.platformIds = "Please insert videogame platform.";
-  } else if (initialValues.platformIds.length > 6){
-    errors.platformIds = "You cannot select more than 6 types of platformIds";
-  }
-if (!initialValues.name) {
-    errors.name = "Please insert videogame name.";
-}
-if (
-    !lettersOrSpacesREGEX.test(initialValues.name) &&
-    initialValues.name
-) {
-    errors.name ='Name is not valid';
-}
-if (!initialValues.description) {
-  errors.description ="Please insert videogame description.";
-}
-if (initialValues.description.length > 200) {
-  errors.description ="Description should be less than 200 characters.";
-  }
-if (!initialValues.price) {
-    errors.price = "Please insert videogame price.";
-}
-if (initialValues.price && !numberREGEX.test(initialValues.price)) {
-    errors.price = "Just numbers.";
-}
-if (!initialValues.released) {
-  errors.released = "Please insert videogame released.";
-}
-
-
-  return errors;
-}
-
 import styles from "./PublishGame.module.css";
 
 
 const PublishGame = () => {
-  const genresList = useSelector((state) => state.genres);
-  const platformsList = useSelector((state) => state.platforms);
 
-  const [previewImage, setPreviewImage] = useState("");  
-  const [formData, setFormData] = useState({
-    name: "",
-    description: "",
-    image: "",
-    price: "",
-    released: "",
+  const dispatch = useDispatch() 
+  const platforms = useSelector(state => state.platforms)
+  const genres = useSelector(state => state.genres)
+  const allVG = useSelector( state => state.games )
+  const allnames = allVG.map( vg => vg.name)
+  //const url = 'https://images.pexels.com/photos/1670977/pexels-photo-1670977.jpeg?'
+  const currentDate = new Date();
+  
+  const [state, setState] = useState({
+    name: '',
+    description: '',
+    image: '',
+    released: '',
+    price: 0,
     isActive: false,
-    genreIds: [],
-    platformIds: [],
-  });
-
-  const [errors, setErrors] = useState({});
-  const dispatch = useDispatch();
+    genres: [],
+    platforms:[]
+  })
+  
+  const [errors, setErrors] = useState({
+    name: 'campo requerido* ',
+    description: 'campo requerido* ',
+    image: '',
+    released: 'campo requerido* ',
+    price: 'campo requerido* ',
+    genres: 'campo requerido* ',
+    platforms:'campo requerido* '
+  })
 
   useEffect(() => {
+    
     dispatch(gameGenres());
     dispatch(gamePlataforms());
-  }, [dispatch]);
-
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file });
-    setPreviewImage(URL.createObjectURL(e.target.files[0]));
-  };
-
-  const handleRemoveImage = () => {
-    const fileInput = document.getElementById("image");
-    fileInput.value = "";
-    setPreviewImage("");
-  };
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData)=> ({...prevFormData, [name]: value}));
-    const validateErr = validate({...formData, [name]: value })
-    setErrors(validateErr);
-  };
-
-  const handlerSelecGenres =(event)=> {
-    const selectedId = event.target.value;
-    if (!formData.genreIds.includes(selectedId)) {
-        setFormData((prevFormData) => ({ ...prevFormData, genreIds: [...prevFormData.genreIds, selectedId] }));
-    }
-  };
-  const handlerSelecPlatform =(event)=> {
-    const selectedId = event.target.value;
-    if (!formData.platformIds.includes(selectedId)) {
-        setFormData((prevFormData) => ({ ...prevFormData, platformIds: [...prevFormData.platformIds, selectedId] }));
-    }
-  };
-   
-  const handlerDeletGenre = (id)=> {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      genreIds: prevFormData.genreIds.filter((g) => g !== id)
-    }));
-    
-  }
-  const handlerDeletPlatf = (id)=> {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      platformIds: prevFormData.platformIds.filter((p) => p !== id)
-    }));
-    
-  }
-  const handleDropdownToggleG = () => {
-     document.getElementById('genreIds');
-  }; 
-  const handleDropdownToggleP = () => {
-    document.getElementById('platformIds');
-  }; 
- 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    let aux = Object.keys(errors);
-    if (aux.length === 0) { 
-           
-      const productData = {
-        name: formData.name ,
-        description: formData.description,
-        image: formData.image || "",
-        price: formData.price || 0,
-        released: formData.released || "",
-        isActive: formData.isActive || false,
-        genreIds: formData.genreIds || [],
-        platformIds: formData.platformIds || [],
-      }      
-       dispatch(createVideogame(productData)).then(() => {
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Succesfully created",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      });
-        
-        setFormData({
-        name: "",
-        description: "",
-        image: "",
-        price: "",
-        released: "",
-        isActive: false,
-        genreIds: [],
-        platformIds: [],
-        });
-        setErrors(validate(productData));
-        setPreviewImage("");
-    } else {
-      alert('Please fix validation errors before submitting');
-    }
-     
-   }
   
+    return () => {
+      
+      
+    };
+  },[])
+  
+  const validate = (state, name) => {
+    switch(name){
+      case `name`:
+        if(state.name === "") setErrors({...errors, name: 'campo requerido*'})
+        else if(state.name.length<4 || state.name.length>20) setErrors({...errors, name: 'el nombre debe tener entre 3 y 20 caracteres'})
+        else if(!/^[a-zA-Z0-9\-": ]+$/.test(state.name)) setErrors({...errors, name: 'solo usar simbolos alfanuméricos, "", - y :'})
+        else if(allnames.includes(state.name)) setErrors({...errors, name: 'este nombre ya existe'})
+        else setErrors({...errors, name: ''}); break
+      
+      case `description`:
+        if(state.description === "") setErrors({...errors, description: 'campo requerido*'})
+        else if(state.description.length<16 || state.description.length>300) setErrors({...errors, description: 'el nombre debe tener entre 15 y 300 caracteres'})
+        else if(!/^[a-zA-Z0-9\-": ]+$/.test(state.description)) setErrors({...errors, description: 'solo usar simbolos alfanuméricos, "", - y :'})
+        else setErrors({...errors, description: ''}); break
+
+      /*case 'image':
+        if(state.image === "") setErrors({...errors, image: 'campo requerido*'})
+        else if(!/^(https?|ftp):\/\/\S+$/.test(state.image)) setErrors({...errors, image: 'La URL de la imagen no es válida.'})
+        else setErrors({...errors, image: ''}); break*/
+
+      case `released`:
+        if(state.released === "") setErrors({...errors, released: 'campo requerido*'})
+        else if(!/^\d{4}-\d{2}-\d{2}$/.test(state.released)) setErrors({...errors, released: 'El formato de fecha debe ser yyyy-mm-dd'})
+        else if((new Date(state.released))> currentDate) setErrors({...errors, released: 'No se permiten fechas futuras.'})
+        else setErrors({...errors, released: ''}); break
+
+      case `price`:
+        if(state.price === "") setErrors({...errors, price: 'campo requerido*'})
+        else setErrors({...errors, price: ''}); break
+
+      case 'genres':
+        state.genres.length === 0
+          ? setErrors({
+              ...errors,
+              genres: 'campo requerido* ',
+            })
+          : setErrors({
+              ...errors,
+              genres: '',
+            });
+        break;
+  
+      case 'platforms':
+        state.platforms.length === 0
+          ? setErrors({
+              ...errors,
+              platforms: 'campo requerido* ',
+            })
+          : setErrors({
+              ...errors,
+              platforms: '',
+            });
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleInputChange = (event) =>{
+    switch(event.target.name){
+      case 'platformIds':
+        let pValue = document.getElementById('platformIds').value
+        if(state.platforms.includes(pValue)) break;//votar error
+        setState({
+          ...state,
+          platforms: [...state.platforms, pValue]
+        })
+        validate({
+          ...state,
+          platforms: [...state.platforms, pValue]
+        },'platforms')
+        break;
+      
+      case 'genreIds':
+        let gValue = document.getElementById('genreIds').value
+        if(state.genres.includes(gValue)) break;//votar error
+        setState({
+          ...state,
+          genres: [...state.genres, gValue]
+        })
+        validate({
+          ...state,
+          genres: [...state.genres, gValue]
+        },'genres')
+        break;
+
+      case 'isActive':
+        const isActive = document.getElementById('isActive')
+        
+        setState({
+          ...state,
+          isActive: isActive.checked
+        })
+        /*validate({
+          ...state,
+          isActive: !isActive.value
+        }, 'isActive')*/
+        break;
+
+      /*case 'image':
+        claudinary*/
+      case 'price':
+        setState({ 
+          ...state,
+          price: event.target.value
+        })
+        validate({ 
+          ...state,
+          price: event.target.value
+        }, 'price')
+
+      default:
+        setState({ 
+          ...state,
+          [event.target.name]: event.target.value
+        })
+        validate({ 
+          ...state,
+          [event.target.name]: event.target.value
+        }, event.target.name)
+    }
+
+  }
+
+  const handleRemove = (event) => {
+    setState({
+      ...state,
+      platforms: [...state.platforms].filter(p => p !== event.target.id),
+      genres: [...state.genres].filter(g => g !== event.target.id)
+    })
+    validate({
+      ...state,
+      platforms: [...state.platforms].filter(p => p !== event.target.id),
+      genres: [...state.genres].filter(g => g !== event.target.id)
+    }, event.target.name)
+  }
+
+  const disableHandler = () => {
+    let totalError = '';
+    for(let i in errors){
+      totalError += errors[i]
+    }
+    return totalError.length? true: false
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('hola');
+    dispatch(createVideogame({
+      ...state,
+      genres: state.genres.map( g => genres.indexOf(g)+1),
+      platforms: state.platforms.map( p => platforms.indexOf(p)+1)
+    }))
+  }
+
 
   return (
     
@@ -181,7 +212,6 @@ const PublishGame = () => {
       <form
         className={styles.createform}
         onSubmit={handleSubmit}
-        
       >
         <div className={styles.group}>
           <label htmlFor="name">Name:</label>
@@ -189,7 +219,7 @@ const PublishGame = () => {
             type="text"
             id="name"
             name="name"
-            value={formData.name}
+            value={state.name}
             onChange={handleInputChange}
             required
           />
@@ -200,7 +230,7 @@ const PublishGame = () => {
           <textarea
             id="description"
             name="description"
-            value={formData.description}
+            value={state.description}
             onChange={handleInputChange}
             required
           ></textarea>
@@ -212,14 +242,14 @@ const PublishGame = () => {
           <label htmlFor="image">Imagen:</label>
           <input
             className={styles.selectimg}
-            type="file"
+            type="text"
             id="image"
             name="image"
-            onChange={handleImageChange}
+            onChange={handleInputChange}
             accept="image/*"
-            required
+            value=''
           />
-          <label  htmlFor="image">
+          {/* <label  htmlFor="image">
             <span className={styles.uploadButton}>Select file</span>
           </label>
           {previewImage && (
@@ -229,36 +259,33 @@ const PublishGame = () => {
               src={previewImage}
               alt="Preview"
             />
-          )}
+          )} 
           <button
             className={styles.buttonDelete}
             type="button"
             onClick={handleRemoveImage}
           >
             Delete image
-          </button>
+          </button>*/}
         </div>
-       
         <div className={styles.group}>
           <label htmlFor="price">Price:</label>
           <input
-            type="text"
+            type="number"
             id="price"
             name="price"
-            value={formData.price}
+            value={state.price}
             onChange={handleInputChange}
-            
           />
           {errors.price && <p className={styles.error}>{errors.price}</p>}
         </div>
-        
         <div className={styles.group}>
           <label htmlFor="released">Released:</label>
           <input
             type="date"
             id="released"
             name="released"
-            value={formData.released}
+            value={state.released}
             onChange={handleInputChange}
           />
           {errors.released && (
@@ -266,100 +293,89 @@ const PublishGame = () => {
           )}
           
         </div>
-          <div className={styles.group}>
+        <div className={styles.group}>
           <label htmlFor="isActive">Active:</label>
           <input
             type="checkbox"
             id="isActive"
             name="isActive"
-            checked={formData.isActive}
-            onChange={() =>
-              setFormData({ ...formData, isActive: !formData.isActive })
-            }
+            checked={state.isActive}
+            onChange={handleInputChange}
           />
         </div>
 
         <div className={styles.group}>
           <label htmlFor="genreIds">Select genres:</label>
-          <div onClick={handleDropdownToggleG}>
+          <div>
               <select
                 id="genreIds"
                 name="genreIds"
-                onChange={handlerSelecGenres}
+                onChange={handleInputChange}
                 defaultValue='default'
               >
                 <option disabled value="default">Genres</option>
-                {genresList?.map((genre) => (
+                {genres?.map((genre) => (
                   <option key={genre} value={genre}>
                     {genre}
                   </option>
                 ))}
               </select>
               <div className={styles.contentSelecPlatfGn}>
-                    {formData.genreIds.map((selectedId)=>{
+                    {state.genres.map((selectedId)=>{
                       
                       return (
                           <div key={selectedId} className={styles.selectedTemp}>
                               {selectedId}
-                              <button type="button" onClick={() => handlerDeletGenre(selectedId)}>
+                              <button type="button" name="genres" id={selectedId} onClick={handleRemove}>
                                   ✖
                               </button>
                           </div>)
                           })}
                </div>
           </div>
-              {errors.genreIds && (
-            <p className={styles.error}>{errors.genreIds}</p>
+              {errors.genres && (
+            <p className={styles.error}>{errors.genres}</p>
           )} 
         </div>
         <div className={styles.group}>
-          <label htmlFor="genreIds">Select platforms:</label>
-          <div onClick={handleDropdownToggleP}>
+          <label htmlFor="platformIds">Select platforms:</label>
+          <div >
               <select
                 id="platformIds"
                 name="platformIds"
-                onChange={handlerSelecPlatform}
+                onChange={handleInputChange}
                 defaultValue='default'
               >
                 <option disabled value="default">Platforms</option>
-                {platformsList?.map((platf) => (
+                {platforms?.map((platf) => (
                   <option key={platf} value={platf}>
                     {platf}
                   </option>
                 ))}
               </select>
              <div className={styles.contentSelecPlatfGn}>
-                 {formData.platformIds.map((selectedId)=>{
+                 {state.platforms.map((selectedId)=>{
                    
                    return (
                        <div key={selectedId} className={styles.selectedTemp}>
                            {selectedId}
-                           <button type="button" onClick={() => handlerDeletPlatf(selectedId)}>
+                           <button type="button" name="platforms" id={selectedId} onClick={handleRemove}>
                                ✖
                            </button>
                        </div>)
                        })}
-        {errors.platformIds && (
-            <p className={styles.error}>{errors.platformIds}</p>
+        {errors.platforms && (
+            <p className={styles.error}>{errors.platforms}</p>
           )} 
               </div> 
           </div>
         </div>
        
           <div className={styles.group}>
-          <button type="submit">Create videogame</button>
+          <button type="submit" >Create videogame</button>
         </div>
       </form>
     </div>
   );
 };
 export default PublishGame
-
-
-
-
-
-
-
-
-
