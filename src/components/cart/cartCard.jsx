dc129dc723075ac69611e11c3d4372820f9f7d52
JuanP <juanpajueloch@gmail.com>
2023-11-14 19:17:18 -0500

    redirects

--- src/components/cart/cartCard.jsx
+++ src/components/cart/cartCard.jsx
@@ -6,7 +6,7 @@ import {
   Grid,
 } from "@mui/material";
 import { Link } from "react-router-dom";
-import BotonsCart from "./botonsCart.jsx";
+import BotonsCart from "./BotonsCart.jsx";
 import PropTypes from "prop-types";
 
 
